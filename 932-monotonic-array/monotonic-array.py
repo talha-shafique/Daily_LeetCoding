835c7dfe70429edf5d8a945916c33345d1d07e87
Talha Shafique <talha.shafique126@gmail.com>
2024-12-01 20:30:54 +0500

    Time: 102 ms (8.10%) | Memory: 29.4 MB (37.43%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -1,10 +1,7 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        if nums==sorted(nums) or nums==sorted(nums, reverse=True):
-            return True
-        else:
-            return False
-        
+        return nums==sorted(nums) or nums==sorted(nums, reverse=True)
+
 
 
 
