a0ef4c12dbea05da0ca3613a8f4ed10663a5fd54
Talha Shafique <talha.shafique126@gmail.com>
2024-12-02 16:31:29 +0500

    Time: 3 ms (8.67%) | Memory: 17.3 MB (5.07%) - LeetSync

--- 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.py
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPrefixString(self, s: str, words: List[str]) -> bool:
-        # if len(words)==1 and s==words[0]:
-        #     return True
         temp=''
         for i in range(len(words)):
             temp=temp+words[i]
