fd77e1bf3a4b9c25264fc63ebe9be5f2560aa44d
Talha Shafique <talha.shafique126@gmail.com>
2024-12-04 01:54:30 +0500

    Time: 2 ms (52.65%) | Memory: 17.6 MB (5.05%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.py
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -3,13 +3,8 @@ class Solution:
         stack=[]
         i=0
         for j in pushed:
-            print('j',j)
             stack.append(j)
             while stack and stack[-1]==popped[i]:
                 stack.pop()
-                print(stack)
                 i=i+1
-        if stack:
-            return False
-        else:
-            return True
+        return not stack
