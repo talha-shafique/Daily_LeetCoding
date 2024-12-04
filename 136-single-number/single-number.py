bf68eae11d100ae3f2bb5108e5eeb1c489fc5f1e
Talha Shafique <talha.shafique126@gmail.com>
2024-12-04 19:47:53 +0500

    Time: 2429 ms (9.90%) | Memory: 19.5 MB (5.39%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,30 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        stack=[]
+        s=[]
         for i in nums:
-            if nums.count(i)==1:
-                return i
+            print('ind',nums.index(i))
+            if i in s:
+                s.remove(i)
+            else:
+                s.append(i)
+        print(s)
+        return s[0]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Less Efficient
+        # for i in nums:
+        #     if nums.count(i)==1:
+        #         return i
 
         
