66f1560c5a12f9fd72dde2cb0da9a53ca04ebb0d
Talha Shafique <talha.shafique126@gmail.com>
2024-12-04 15:53:36 +0500

    Time: 78 ms (56.94%) | Memory: 18.1 MB (5.69%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,34 +1,21 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
         p=0
-        s=''
+        res=''
         if len(str1)<len(str2):
             return False
         else:
             for i in str1:
-                print('i',i)
                 while p<len(str2):
-                    print('p',p)
                     if i=='z' and str2[p]=='a':
-                        s=s+str2[p]
-                        p=p+1
+                        res=res+str2[p]; p=p+1
                         break
                     elif i==str2[p] or ord(i)+1==ord(str2[p]):
-                        print('orig i',i)
-                        s=s+str2[p]
-                        print('updated i',i)
-                        p=p+1
+                        res=res+str2[p]; p=p+1
                         break
                     else:
-                        print('no luck!')
                         break
-            
-        print(s)
-        print(str2)
-        if s == str2:
-            return True
-        else:
-            return False
+        return res==str2
 
         
 
