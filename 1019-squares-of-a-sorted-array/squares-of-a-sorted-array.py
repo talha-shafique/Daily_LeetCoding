ae2aa8d21886e9e872424c1c92280aac188db0da
Talha Shafique <talha.shafique126@gmail.com>
2024-12-05 15:38:01 +0500

    Time: 7 ms (80.18%) | Memory: 19 MB (7.31%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([x**2 for x in nums])
+        # res=[]
+        # for i in nums:
+        #     i=i**2
+        #     res.append(i)
+        # res.sort()
+        # return res
+        
